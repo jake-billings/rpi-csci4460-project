89733f256638b22f51cf441584205e04f308896b
Jake Billings <jake@jakebillings.com>
2020-12-04 09:26:36 -0700

    Add: logging

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const config = require('./config')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
+// Add logging
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 /**
  * rank_of()
